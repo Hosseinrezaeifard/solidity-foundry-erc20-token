f91d9c87dba8914dd59ac443a0d209f565e58622
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-01 12:26:34 -0700

    Make run function return token instance in deploy script

--- script/DeployZXE.s.sol
+++ script/DeployZXE.s.sol
@@ -7,9 +7,10 @@ import {ZXE} from "../src/ZXE.sol";
 contract DeployZXE is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (ZXE) {
         vm.startBroadcast();
-        new ZXE(INITIAL_SUPPLY);
+        ZXE zxeToken = new ZXE(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return zxeToken;
     }
 }
